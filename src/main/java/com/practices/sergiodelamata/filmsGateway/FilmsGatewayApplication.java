b4644b468b36929614b01b77aef4ec68b83e9054
SergiodelaMata <sergio.matam@edu.uah.es>
2022-02-02 17:56:17 +0100

    :sparkles: Established the structure of the Spring Boot project.

---
+++ src/main/java/com/practices/sergiodelamata/filmsGateway/FilmsGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.practices.sergiodelamata.filmsGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmsGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practices/sergiodelamata/filmsGateway/FilmsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practices.sergiodelamata.filmsGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
